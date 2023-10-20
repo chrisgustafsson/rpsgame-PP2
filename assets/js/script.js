96b12af9e7c71c81c2176eb96208d3763bbe8450
chrisgustafsson <>
2023-10-20 15:53:31 +0000

    ADD functionality for AI pick

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,12 @@ const picks = ["rock", "scizor", "paper", "lizard", "spock"];
 let userScore = 0;
 let aiScore = 0;
 
+// Functionality for AI pick
+function aiPick() {
+    const randomPick = Math.floor(Math.random() * picks.length);
+    return picks[randomPick];
+}
+
 // Rules of the game itself
 const rules = {
     rock: ["scizor", "lizard"],
