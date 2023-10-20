6505d3e2c993b884521665cddc14c511a783d98c
chrisgustafsson <>
2023-10-20 15:31:46 +0000

    ADD game rules to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,12 @@
 const picks = ["rock", "scizor", "paper", "lizard", "spock"];
 let userScore = 0;
 let aiScore = 0;
+
+// Rules of the game itself
+const rules = {
+    rock: ["scizor", "lizard"],
+    scizor: ["paper", "lizard"],
+    paper: ["rock", "spock"],
+    lizard: ["spock", "paper"],
+    spock: ["scizor", "rock"],
+};
