f663f7cc1ddd63703bd00b5c4a0b607a4027655c
chrisgustafsson <>
2023-10-20 16:13:21 +0000

    ADD functionality for declaring a winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,29 @@ function aiPick() {
     return picks[randomPick];
 }
 
-// Rules of the game itself
-const rules = {
-    rock: ["scizor", "lizard"],
-    scizor: ["paper", "lizard"],
-    paper: ["rock", "spock"],
-    lizard: ["spock", "paper"],
-    spock: ["scizor", "rock"],
-};
+// Functionality for deciding the winner
+function decideWinner(userPick, aiPick) {
+    if (userPick === aiPick) {
+        return "tie";
+    }
+
+    // Rules of the game itself, first item beats the contained items
+    const rules = {
+        rock: ["scizor", "lizard"],
+        scizor: ["paper", "lizard"],
+        paper: ["rock", "spock"],
+        lizard: ["spock", "paper"],
+        spock: ["scizor", "rock"],
+    };
+
+    if (rules[userPick].includes(aiPick)) {
+        return "user";
+    } else {
+        return "ai";
+    }
+}
+
+
 
 // Event listener for click on the potential picks
 const buttons = document.querySelectorAll('.buttons');
